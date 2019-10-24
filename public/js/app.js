f8891c1c5800e838d809d8ae5c195a07f720be8b
Ryan Thatcher <ryanthatcher@Ryans-MacBook-Air.local>
2019-10-24 14:29:51 -0700

    Remove console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-//console.log('Client side javascript file is loaded')
-
 const weatherForm = document.querySelector('form') // yields JSON of form
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
